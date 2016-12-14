7ece0aff8ad5079d4e8faa8c64a31a6d3a4c2e22
muehan <andre.muehle@pilatus-aircraft.com>
2016-12-14 15:34:41 +0100

    make the manu active working, not sure if this is realy best practice...

---
+++ HomeConsole/static/js/menu.js
@@ -0,0 +1,25 @@
+$(function(){
+    'use strict'
+
+    var currentHref = window.location.pathname.toLowerCase().replace("/", "");
+    if(!currentHref){
+        currentHref = "overview";
+    }
+    
+    var ulElements = document.getElementsByClassName("nav");
+
+    var oldMenuActive = document.getElementsByClassName("active");
+    oldMenuActive.className = "";
+    
+    for (var x = 0; x < ulElements.length; x++) {
+        for (var i = 0; i < ulElements[x].childNodes.length; i++) {
+            var liElement = ulElements[x].childNodes[i];
+            if(liElement.innerText){
+                if(liElement.innerText.trim().toLowerCase() == currentHref){
+                    liElement.className += " active";
+                    break;
+                }
+            }
+        }
+    }
+});
